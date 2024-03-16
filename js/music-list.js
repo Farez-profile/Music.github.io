d1370055867489c104a69c20e308e5af9c017539
Farez-profile <163609181+Farez-profile@users.noreply.github.com>
2024-03-16 08:31:34 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Rumah Ke Rumah",
+    artist: "Hindia",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Secukupnya",
+    artist: "Hindia",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Halu",
+    artist: "Feby Putri",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Usik",
+    artist: "Feby Putri",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Resah Jadi Luka",
+    artist: "Daun Jatuh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Sesuatu Di Jogja",
+    artist: "Adhitia Sofyan",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Nanti Kita Seperti ini",
+    artist: "Batas Senja",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Serana",
+    artist: "For Revenge",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Monolog",
+    artist: "Pamungkas",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "One Only",
+    artist: "Pamungkas",
+    img: "music-10",
+    src: "music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
